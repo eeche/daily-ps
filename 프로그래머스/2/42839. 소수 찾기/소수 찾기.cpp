e620e68a7b308ffff63c2c0ad1031e2df222278c
eeche <dlckdgus200011@gmail.com>
2025-03-20 11:10:12 +0900

    [level 2] Title: 소수 찾기, Time: 17.14 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -2,13 +2,20 @@
 
 using namespace std;
 
-// bool is_prime(int num) {
-//     if (num < 2) return false;
-//     for (int i = 2; i <= sqrt(num); i++) {
-//         if (num % i == 0) return false;
-//     }
-//     return true;
-// }
+vector<bool> sieve(int max_num) {
+    vector<bool> is_prime(max_num + 1, true);
+    
+    is_prime[0] = is_prime[1] = false;
+    
+    for (int i = 2; i * i <= max_num; i++) {
+        if (is_prime[i]) {
+            for (int j = i * i; j <= max_num; j += i) {
+                is_prime[j] = false;
+            }
+        }
+    }
+    return is_prime;
+}
 
 int solution(string numbers) {
     int answer = 0;
@@ -25,24 +32,11 @@ int solution(string numbers) {
     } while (next_permutation(numbers.begin(), numbers.end()));
     
     int max_num = *num_set.rbegin();
-    vector<bool> is_prime(max_num + 1, true);
-    
-    is_prime[0] = is_prime[1] = false;
-    
-    for (int i = 2; i * i <= max_num; i++) {
-        if (is_prime[i]) {
-            for (int j = i * i; j <= max_num; j += i) {
-                is_prime[j] = false;
-            }
-        }
-    }
+    vector<bool> is_prime = sieve(max_num);
     
     for (int num : num_set) {
         if (is_prime[num]) answer++;
     }
     
-//     for (int num : num_set) {
-//         if (is_prime(num)) answer++;
-//     }
     return answer;
 }
