c7451eedd6b81db2626b27dd27635be5b4117503
Changhyun Lee <dlckdgus200011@gmail.com>
2025-04-10 23:30:39 +0900

    Time: 0 ms (100.00%) | Memory: 8.5 MB (81.99%) - LeetSync

---
+++ LeetCode/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if (haystack.size() >= needle.size()) {
+            for (int i = 0; i <= haystack.size()-needle.size(); ++i) {
+                if (haystack.substr(i, needle.size()) == needle) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+};
